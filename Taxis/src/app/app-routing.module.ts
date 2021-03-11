04b35b84d3e689405a0286df17acd55745e97013
cacike69 <57674244+cacike69@users.noreply.github.com>
2021-03-11 17:18:31 +0100

    Columnas e imagenes in process

--- Taxis/src/app/app-routing.module.ts
+++ Taxis/src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
+import { IniciComponent } from './inici/inici.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+{ path: 'inici', component: IniciComponent }, //aqui vamos a poner las rutas de los componentes
+
+];
 
 @NgModule({
+
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
